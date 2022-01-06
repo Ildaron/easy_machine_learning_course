91e1e6541bdac11b15a9b0c6153653122323407f
Ildaron <ildar.o2010@yandex.ru>
2022-01-06 22:35:47 +0900

    Update 1.Keras.py

--- Kaggle/1.House_Prices/1.House_Prices/1.Keras.py
+++ Kaggle/1.House_Prices/1.House_Prices/1.Keras.py
@@ -60,7 +60,7 @@ model.add(Dense(200, input_dim=78, kernel_initializer='normal', activation='relu
 model.add(Dense(100, kernel_initializer='normal', activation='relu'))
 model.add(Dense(50, kernel_initializer='normal', activation='relu'))
 model.add(Dense(25, kernel_initializer='normal', activation='relu'))
-model.add(Dense(1, kernel_initializer='normal'))
+model.add(Dense(1, kernel_initializer='normal')) #activation='softmax'
 model.summary()
 
 #visualizer(model, format='png', view=True)
