147682cede3eb8f4f50a48ac34a9189ac5b593e2
Ildaron <ildar.o2010@yandex.ru>
2022-01-10 23:21:48 +0900

    Update 1.House_Prices.py

--- Kaggle/1.House_Prices/1.House_Prices.py
+++ Kaggle/1.House_Prices/1.House_Prices.py
@@ -1,104 +1,119 @@
+import keras
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.models import Sequential
+#from keras_visualizer import visualizer 
+from sklearn.metrics import mean_squared_log_error as msle, mean_squared_error as mse, make_scorer
 
-
-# 1 data prepairing
+#1. Data prepairing
 data_train = pd.read_csv('train.csv')
-data_test = pd.read_csv('test.csv')
+samples_to_predict = pd.read_csv('test.csv')
+l=list(range(0,1459)) 
+samples_to_predict["C"]= l
 
-data_train=data_train.fillna('0')
+def data_all (data_train):
+ data_train=data_train.fillna('0')
+ data_train_col=data_train.values
+ col_len=(len(data_train_col[0]))
+ count=0
+ col=[]
 
-data_train_col=data_train.values
-col_len=(len(data_train_col[0]))
-count=0
-col=[]
+ for a in range(0,col_len,1):
+  type_collom=type(data_train_col[0,a])
+  if  (type_collom == str):  # de
+   col=np.append(col, int(count))
+  count=count+1
 
-for a in range(0,col_len,1):
- type_collom=type(data_train_col[0,a])
- if  (type_collom == str):  # de
-  col=np.append(col, int(count))
- count=count+1
+ col = list(map(int, col))
+ col_re = list(data_train.columns.values)
 
-col = list(map(int, col))
-col_re = list(data_train.columns.values)
+ name= []
+ for a in col:
+  name.append(col_re[a])
 
-name= []
-for a in col:
- name.append(col_re[a])
+ for a in name:
+  all_names = set(data_train[a]) # replace N/A by A
+  unique = {k: i for i, k in enumerate(all_names)} # create unique names for replace str 
+  data_train[a] = data_train[a].map(unique)
 
-for a in name:
-   # replace N/A by A
- all_names = set(data_train[a])
- unique = {k: i for i, k in enumerate(all_names)}
- data_train[a] = data_train[a].map(unique)
+ data_train=data_train.values
+ x = data_train[:,1:79]
+ y = data_train[:,80]
 
-#data_train=data_train.values
-np.set_printoptions(threshold=np.inf)  
+ x = x.astype(int)
+ y = y.astype(int)
+ dataset = dict()
+ dataset["x"]=x
+ dataset["y"]=y
+ return dataset
+
+data=data_all(data_train)
+x=data["x"]
+y=data["y"]
+
+data_test=data_all(samples_to_predict)
+data_test_x=data_test["x"]
+print (data_test_x)
+
+#2. Data processing
+from sklearn import preprocessing
+min_max_scaler = preprocessing.MinMaxScaler()
 
 
-
-#data_train=data_train.fillna(1)
-
-data_train=data_train.values
-x = data_train[:,1:79]
-y = data_train[:,80]
-
+x = min_max_scaler.fit_transform(x)
+y= y.reshape(-1, 1)
+y = min_max_scaler.fit_transform (y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
 
-x_train = x_train.astype(np.int)
-y_train = y_train.astype(np.int)
-#print (len(x_train))
-#print (len(y_train))
+#3. Model
+#3.1 Create model
+model = Sequential()
+model.add(Dense(200, input_dim=78, kernel_initializer='normal', activation='relu')) # input_dim=78,
+model.add(Dense(100, kernel_initializer='normal', activation='relu'))
+model.add(Dense(50, kernel_initializer='normal', activation='relu'))
+model.add(Dense(25, kernel_initializer='normal', activation='relu')) #activation='softmax'   activation='sigmoid'
+model.add(Dense(1, kernel_initializer='normal')) 
+model.summary()
+
+#visualizer(model, format='png', view=True)
+
+#3.2 Compile
+#model.compile(loss='mean_squared_error', metrics=['acc'], optimizer=keras.optimizers.Adadelta())
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mae']) # metrics=['mae']
+
+#model.compile(optimizer=tf.train.AdamOptimizer(),loss='mse', metrics=['acc'])
+
+#3.3 Train
+#early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+hist = model.fit (x_train, y_train,  batch_size = 32 , epochs = 100) #history = m
+
+#model.fit(validation_split=0.1)
+#3.4 Evaluate the model
+test_mse_score, test_mae_score = (model.evaluate (x_test, y_test))
 
 
+predictions = model.predict(data_test_x)
+dframe = pd.DataFrame(predictions) 
+dframe.to_excel('./teams.xlsx')
 
-# 2 Data processing
+predictions = model.predict(x_test)
+print("loni", tf.keras.metrics.mean_squared_logarithmic_error(y_test, predictions))
+print ("msle", msle(predictions, y_test))
 
-#3. Machine learning
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
+#print (test_mse_score)
+#print (test_mae_score)
+import matplotlib.pyplot as plt
+#plt.plot(hist.history['loss'])
+plt.plot(hist.history['mae'])
+plt.title('Model loss')
+plt.ylabel('mae')
+plt.xlabel('epoch')
+plt.legend(['Loss', 'acc'], loc='upper right')
+plt.show()
 
-#from sklearn.tree import RandomForestClassifier
-
-# fit a CART model to the data
-model = RandomForestClassifier(n_estimators=10, min_samples_split=3)
-
-                                  # class sklearn.ensemble.RandomForestClassifier(n_estimators=10,
-                                  #                                              criterion='gini', 
-                                  #                                               max_depth=None,
-                                  #                                               min_samples_split=2,
-                                  #                                               min_samples_leaf=1, 
-
-                                  #                                               min_weight_fraction_leaf=0.0, 
-                                  #                                               max_features='auto', 
-                                  #                                               max_leaf_nodes=None, 
-                                  #                                               bootstrap=True, 
-                                  #                                               oob_score=False,
-                                  #                                               n_jobs=1, 
-                                  #                                               random_state=None,
-                                  #                                               verbose=0, 
-                                  #                                               warm_start=False, 
-                                  #                                               class_weight=None)
-
-
-model.fit(x_train, y_train)
-
-
-#print(model)
-# make predictions
-
-expected = y_test
-predicted = model.predict(x_test)
-# summarize the fit of the model
-#print(metrics.classification_report(expected, predicted))
-#print(metrics.confusion_matrix(expected, predicted))
-from sklearn.metrics import accuracy_score  
-
-print (type(predicted))
-print ("ok")
-print (type(y_test))
-
-z=accuracy_score(list(y_test), list(predicted))
-print (z)
+model.predict(x_test)
