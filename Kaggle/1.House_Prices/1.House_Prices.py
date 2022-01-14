6286203bf7d7b39b3c52d2223279d9dddb5a2007
Ildaron <ildar.o2010@yandex.ru>
2022-01-14 14:21:14 +0900

    Update 1.House_Prices.py

--- Kaggle/1.House_Prices/1.House_Prices.py
+++ Kaggle/1.House_Prices/1.House_Prices.py
@@ -1,3 +1,4 @@
+# hyperparameter
 # GRAPT - ACTUAL AND PREDICTED
 # try  Gradient Boosting Regressor # Random Forest Regressor # Ridge regression (L2) # Lasso regression (L1)
 # feature selection methods
