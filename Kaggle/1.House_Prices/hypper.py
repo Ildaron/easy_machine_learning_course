586be3c3806c270b8ab96e1d5bd8296a0d03a7e4
Ildaron <ildar.o2010@yandex.ru>
2022-01-20 12:54:13 +0900

    Update hypper.py

--- Kaggle/1.House_Prices/hypper.py
+++ Kaggle/1.House_Prices/hypper.py
@@ -1,4 +1,3 @@
-#importing necessary libraries
 # https://www.analyticsvidhya.com/blog/2021/08/easy-hyperparameter-tuning-in-neural-networks-using-keras-tuner/
 import pandas as pd
 import numpy as np
@@ -9,81 +8,65 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.datasets import fashion_mnist
+import keras_tuner
+from keras_tuner import RandomSearch
+import keras
 
-(X_train,y_train),(X_test,y_test)=fashion_mnist.load_data()
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.models import Sequential
+from sklearn.metrics import mean_squared_log_error as msle, mean_squared_error as mse, make_scorer
 
-#visualizing the dataset
-for i in range(25):
-    # define subplot
-    plt.subplot(5, 5, i+1)
-    # plot raw pixel data
-    plt.imshow(X_train[i], cmap=plt.get_cmap('gray'))
-# show the figure
-plt.show()
 
-#normalizing the images
-X_train=X_train/255
-X_test=X_test/255
+x_train = pd.read_excel('x_train_after_compil.xlsx').values
+y_train = pd.read_excel('y_train_after_compil.xlsx').values
+x_test = pd.read_excel('x_test_after_compil.xlsx').values
+y_test = pd.read_excel('y_test_after_compil.xlsx').values
 
-model=Sequential([
-    #flattening the images
-    Flatten(input_shape=(28,28)),
-    #adding first hidden layer
-    Dense(256,activation='relu'),
-    #adding second hidden layer
-    Dense(128,activation='relu'),
-    #adding third hidden layer
-    Dense(64,activation='relu'),
-    #adding output layer
-    Dense(10,activation='softmax')
-])
+x_train = np.delete(x_train, 0, 1)
+y_train = np.delete(y_train, 0, 1)
+y_test = np.delete(y_test, 0, 1)
+x_test = np.delete(x_test, 0, 1)
 
-#compiling the model
-model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-#fitting the model
-model.fit(X_train,y_train,epochs=10)
+print ("x shape", x_test.shape)
+model = Sequential()
+model.add(Dense(200, input_dim=78, kernel_initializer='normal', activation='relu')) 
+model.add(Dense(100, kernel_initializer='normal', activation='relu'))
+model.add(Dense(50, kernel_initializer='normal', activation='relu'))
+model.add(Dense(25, kernel_initializer='normal', activation='relu')) #activation='softmax'   activation='sigmoid'
+model.add(Dense(1, kernel_initializer='normal')) 
+model.summary()
+model.compile(optimizer='rmsprop', loss='mse',metrics=['mean_squared_logarithmic_error'])
 
-#evaluating the model
-model.evaluate(X_test,y_test)
+
+hist = model.fit (x_train, y_train,  batch_size = 32, epochs = 1)
+model.evaluate(x_test,y_test)
 
 
 
 
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,Flatten,Dropout,Dense
+from tensorflow.keras.optimizers import Adam
 
 
-
-def build_model(hp):          #hp means hyper parameters
+def build_model(hp):        
     model=Sequential()
-    model.add(Flatten(input_shape=(28,28)))
-    #providing range for number of neurons in a hidden layer
-    model.add(Dense(units=hp.Int('num_of_neurons',min_value=32,max_value=512,step=32),
-                                    activation='relu'))
-    #output layer
-    model.add(Dense(10,activation='softmax'))
-    #compiling the model
-    model.compile(optimizer=keras.optimizers.Adam(hp.Choice('learning_rate',values=[1e-2, 1e-3, 1e-4])),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    model.add(Dense(200, input_dim=78, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(units=hp.Int('num_of_neurons',min_value=25,max_value=200,step=25),activation='relu'))  #!!!!
+    model.add(Dense(1, kernel_initializer='normal')) 
+    model.compile(optimizer='rmsprop', loss='mse',metrics=['mean_squared_logarithmic_error'])
     return model
 
-
-
-
-#feeding the model and parameters to Random Search
 tuner=RandomSearch(build_model,
-    objective='val_accuracy',
+    objective='mean_squared_logarithmic_error',
     max_trials=5,
     executions_per_trial=3,
     directory='tuner1',
     project_name='Clothing')
 
-
-
-#this tells us how many hyperparameter we are tuning
-#in our case it's 2 = neurons,learning rate
-tuner.search_space_summary()
-
-#fitting the tuner on train dataset
-tuner.search(X_train,y_train,epochs=10,validation_data=(X_test,y_test))
-
+#hp for 2 = neurons,learning rate
+tuner.search_space_summary() 
+tuner.search(x_train,y_train,epochs=1,validation_data=(x_test,y_test)) 
 tuner.results_summary()
-
