d9183d838ddaaafb259f66b5cad6b034cd93f4a1
Ildaron <ildar.o2010@yandex.ru>
2022-02-25 15:56:50 +0900

    Update agent.py

--- Q_Learning/mirror_control/agent.py
+++ Q_Learning/mirror_control/agent.py
@@ -1,8 +1,6 @@
 #https://medium.com/@gtnjuvin/my-journey-into-deep-q-learning-with-keras-and-gym-3e779cc12762
 #import gym #
 import env
-
-
 import random
 import os
 import numpy as np
@@ -10,6 +8,7 @@ from collections      import deque
 from keras.models     import Sequential
 from keras.layers     import Dense
 from tensorflow.keras.optimizers import Adam
+
 one_time=0
 class Agent():
     def __init__(self, state_size, action_size):
@@ -30,7 +29,7 @@ class Agent():
         model.add(Dense(24, activation='relu'))
         
         #model.add(Dense(self.action_size, activation='linear'))  # ПРЕДСКАЗЫВАЕТ ТОЛЬКО ОДНО ЗНАЧЕНИЕ А НАДО ДВА
-        model.add(Dense(2, kernel_initializer='normal')) 
+        model.add(Dense(4, kernel_initializer='normal')) 
 
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
@@ -42,12 +41,12 @@ class Agent():
         
         self.coordnate = self.brain.predict(state) # передаем на прогноз состояние среды
 
-        #print ("coordnate",type (coordnate))
-               
-        #print (" x_offset, y_offset",  x_offset, y_offset)
-        #print ("act_values",act_values)        # 
-        return (self.coordnate[0])          # возвращает влево или в право
+        #self.coordnate =  [0, 0.9, 0.7, 0, 0, 0, 0, 0]
+        #print ("self.coordnate[0]", np.argmax(self.coordnate))      = 1   
 
+        #print (" x_offset, y_offset",  x_offset, y_offset)
+        #print ("act_values",act_values)        #          # возвращает влево или в право
+        return np.argmax(self.coordnate)
 
     def remember(self, state, action, reward, next_state, done):
         
@@ -69,17 +68,22 @@ class Agent():
                       #done from self.env.step(action)
         
     def replay(self, sample_batch_size):  # Эта функция уменьшает разницу между нашим прогнозом и целью на скорость обучения.
-        if len(self.memory) < sample_batch_size:
+        if len(self.memory) < sample_batch_size: # пока 32 раща не сделает, собака,знак лти не наоборот
+            print ("not yet")
             return
         sample_batch = random.sample(self.memory, sample_batch_size)
         print ("sample_batch_start", len (sample_batch)) # запоминает 32 эпизода обучения 
         for state, action, reward, next_state, done in sample_batch:
             target = reward
-            if not done:
-              target = reward + self.gamma * np.amax(self.brain.predict(next_state)[0]) #loss
+            if (done == 0):
+              target = reward + self.gamma * np.amax(self.brain.predict(next_state)[0]) #именно одна целая игра здесь, [0]
+              print ("the attept was finished")
             # Target - уменьшить потери, то есть разрыв между прогнозом и целью.
+            print ("state", state)
             target_f = self.brain.predict(state)
-            target_f[0][action] = target
+            print ("targettttttttttttt", target_f) # [[0. 0.]]   - здесь собака и зарыта, перед трениовуой надло
+            #target_f[0][action] = target
+            
             self.brain.fit(state, target_f, epochs=1, verbose=0)# Керас вычитает target из вывода нейронной сети и возводит ее в квадрат
                                                                 # Эта функция уменьшает разницу между нашим прогнозом и целью на скорость обучения.
                                                                 # И по мере того, как мы повторяем процесс обновления,
@@ -91,8 +95,8 @@ class Agent():
 
 class CartPole:
     def __init__(self):
-        self.sample_batch_size = 32
-        self.episodes          = 290 # Это указывает, сколько игр мы хотим, чтобы агент сыграл, чтобы обучить себя.
+        self.sample_batch_size = 320 #размер партии образца
+        self.episodes          = 600 # Это указывает, сколько игр мы хотим, чтобы агент сыграл, чтобы обучить себя.
         #self.env               = gym.make('CartPole-v1')
 
         self.state_size        = 2 #self.env.observation_space.shape[0]
@@ -111,45 +115,35 @@ class CartPole:
                     one_time=1
                     state = [[0,0]]
 
-         
-                done = False
+                #done = False
                 index = 0
                 #while not done: # Done логическое значение, указывающее, закончилась игра или нет.
                 #self.env.render()
                 action = self.agent.act(state) # y_laser, x_laser #передаем на прогноз состояние среду
-                print ("asasas", action)
 
                 #y_laser =action[0]
                 #x_laser =action[1]
                 #next_state, reward, done, _ = self.env.step(action) # передаем прогноз в среду - и тут фишка, что сама уже среда выдает - next_state, reward, done
-
-
-
-                x_offset=action[0]
-                y_offset=action[1]
-                
+               
                 #import testos
-
                 #print (testos(x_offset, y_offset))
-                x_y_coord = env.camera(x_offset, y_offset)
-                
+                print ("action",action)
+                x_y_coord = env.camera(action) #x_offset, y_offset
                 #reward_x,reward_y, next_state, y_laser, x_laser = env(x_offset, y_offset)
-
                 #reward=reward_x+reward_y
                 reward = x_y_coord[0]
-                next_state = [x_y_coord[3], x_y_coord[4]]
-                print ("ildar", next_state)  # ildar [ 0.04968709 -0.17844233  0.04515736  0.33838844]
-                 
+                next_state = [x_y_coord[2], x_y_coord[3]]
+                print ("next_state", next_state)  # ildar [ 0.04968709 -0.17844233  0.04515736  0.33838844]                 
                 next_state = np.reshape(next_state, [1, self.state_size])
-                print ("ildar1", next_state) # ildar1 [[ 0.04968709 -0.17844233  0.04515736  0.33838844]]
- 
+                print ("next_state_reshape", next_state) # ildar1 [[ 0.04968709 -0.17844233  0.04515736  0.33838844]]
+
+                done =  x_y_coord[1]
                 self.agent.remember(state, action, reward, next_state, done) # 
 
                 state = next_state
                 index += 1
                 print("Episode {}# Score: {}".format(index_episode, index + 1))
-                self.agent.replay(self.sample_batch_size)
-
+                self.agent.replay(self.sample_batch_size) # собака sample_batch_size = 32
 
 if __name__ == "__main__":
     cartpole = CartPole()
