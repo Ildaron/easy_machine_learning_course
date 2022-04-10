501d5b85dd7b95330119d515bbcddac523ddc2df
Ildaron <ildar.o2010@yandex.ru>
2022-04-10 21:39:02 +0100

    Update agent.py

--- Q_Learning/mirror_control/agent.py
+++ Q_Learning/mirror_control/agent.py
@@ -14,8 +14,8 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=1000) #2000
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
+        self.gamma = 0.95                #discount rate
+        self.epsilon = 1.0               #exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
@@ -35,24 +35,23 @@ class DQNAgent:
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        if np.random.rand() <= self.epsilon: #self.epsilon:
-            #print ("not")
+        if np.random.rand() <= self.epsilon: 
             return random.randrange(self.action_size)
         act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+        return np.argmax(act_values[0])  
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
             target = reward
-            #if not done:
-            target = (reward + self.gamma *np.amax(self.model.predict(next_state)[0]))
+            if not done:
+             target = (reward + self.gamma *np.amax(self.model.predict(next_state)[0]))
             # начение Q для определенной пары состояние-действие должно быть наградой,
             # полученной при переходе в новое состояние (путем выполнения этого действия), добавленной к значению наилучшего действия в следующем состоянии.
             # обесценивание будующих наград
-            target_f = self.model.predict(state) #state = next_state
+            target_f = self.model.predict(state)
             target_f[0][action] = target       
-            self.model.fit(state, target_f, epochs=1, verbose=0) # 
+            self.model.fit(state, target_f, epochs=1, verbose=0) 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -64,24 +63,24 @@ class DQNAgent:
 
 if __name__ == "__main__":
     #env = gym.make('CartPole-v1')
-    state_size = 2#env.observation_space.shape[0] #state_size 4
-    action_size = 4#env.action_space.n # action_size 2
-    agent = DQNAgent(state_size, action_size) #agent <__main__.DQNAgent object at 0x00000158F7EDF700>
+    state_size = 2
+    action_size = 4
+    agent = DQNAgent(state_size, action_size) 
     # agent.load("./save/cartpole-dqn.h5")
     done = False
-    batch_size = 16 # 32 #128
+    batch_size = 128 # 32 #128
 
     for e in range(EPISODES):
-        state=[[100,100]] # reset if point in the out of the picture
+        state=[[25,25]] # start point
         state = np.reshape(state, [1, 2])
-        for time in range(5000): #5000
-            action = agent.act(state) #
+        for time in range(5000): 
+            action = agent.act(state) 
             data_from_env = env.camera(action, state)
             reward = data_from_env[0]
             next_state = [data_from_env[2], data_from_env[3]]  # x and y coordinate
             next_state = np.reshape(next_state, [1, 2])
             done =  data_from_env[1]            
-            #reward = reward if not done else -10
+            reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, 2])
             agent.memorize(state, action, reward, next_state, done)
 
@@ -89,7 +88,7 @@ if __name__ == "__main__":
             if done==0:
                 #print("episode: {}/{}, score: {}, e: {:.2}".format(e, EPISODES, time, agent.epsilon))
                 break
-            if len(agent.memory) > batch_size: #batch_size
+            if len(agent.memory) > 900: #batch_size
                 #print ("problem")
                 agent.replay(batch_size)
         # if e % 10 == 0:
