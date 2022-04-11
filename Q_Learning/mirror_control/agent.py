9bfe9d335d3e38c24bc78eaa96270b89e1eb4eee
Ildaron <ildar.o2010@yandex.ru>
2022-04-11 22:59:08 +0100

    Update agent.py

--- Q_Learning/mirror_control/agent.py
+++ Q_Learning/mirror_control/agent.py
@@ -13,7 +13,7 @@ class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=1000) #2000
+        self.memory = deque(maxlen=50001) #2000
         self.gamma = 0.95                #discount rate
         self.epsilon = 1.0               #exploration rate
         self.epsilon_min = 0.01
@@ -43,7 +43,7 @@ class DQNAgent:
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            print ("reward", reward)
+            #print ("reward", reward)
             target = reward
             if not done:
                 
@@ -70,10 +70,10 @@ if __name__ == "__main__":
     agent = DQNAgent(state_size, action_size) 
     # agent.load("./save/cartpole-dqn.h5")
     done = False
-    batch_size = 1 # 32 #128
+    batch_size = 2048 # 32 #128
 
     for e in range(EPISODES):
-        state=[[25,25]] # start point
+        state=[[85,85]] # start point
         state = np.reshape(state, [1, 2])
         for time in range(5000): 
             action = agent.act(state) 
@@ -90,7 +90,7 @@ if __name__ == "__main__":
             if done==0:
                 #print("episode: {}/{}, score: {}, e: {:.2}".format(e, EPISODES, time, agent.epsilon))
                 break
-            if len(agent.memory) > 100: #batch_size
+            if len(agent.memory) > 50000: #batch_size
                 #print ("problem")
                 agent.replay(batch_size)
         # if e % 10 == 0:
