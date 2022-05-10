80a8f1c3b84246f41e44f9c90255633f235ad2fe
Ildaron <ildar.o2010@yandex.ru>
2022-05-10 11:43:55 +0100

    Update 3.Many_graph_in_one.py

--- matplotlib/3.Many_graph_in_one.py
+++ matplotlib/3.Many_graph_in_one.py
@@ -2,13 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 dataset = pd.read_csv("dataset.csv")
 col = list(dataset.columns.values)
+
 b=0
 for a in col:
  if a !="Unnamed: 20":   
   print ("a",a)
-  plt.plot(dataset["Date"],dataset[a])
+  plt.plot(dataset["Date"],dataset[a], label=a)
   b=b+1
-plt.xlabel("nm, data from PIA25", fontsize=8, fontweight="bold")
+plt.legend()  
+plt.xlabel("nm, data", fontsize=8, fontweight="bold")
+plt.ylabel("see legend", fontsize=8, fontweight="bold")
+plt.title('Beamage data analysis')
 plt.show()
 
 
